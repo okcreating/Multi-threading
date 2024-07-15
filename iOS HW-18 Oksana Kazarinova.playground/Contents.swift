f5000fa6ab9a3809182103848194cf8692df1151
okcreating <oksibearus@gmail.com>
2024-07-15 21:23:46 +0100

    Threads adjustments done.

--- iOS HW-18 Oksana Kazarinova.playground/Contents.swift
+++ iOS HW-18 Oksana Kazarinova.playground/Contents.swift
@@ -2,12 +2,6 @@ import Foundation
 import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-var storage = [Chip]()
-var mutex = NSLock()
-var isStorageEmpty: Bool {
-    storage.isEmpty
-}
-
 public struct Chip {
     public enum ChipType: UInt32 {
         case small = 1
@@ -25,52 +19,93 @@ public struct Chip {
     }
 
     public func soldering() {
-        let soderingTime = chipType.rawValue
-        sleep(UInt32(soderingTime))
+        let solderingTime = chipType.rawValue
+        sleep(UInt32(solderingTime))
+    }
+}
+
+class ThreadSafeWorkout {
+    private var storage = [Chip]()
+    var mutex = NSCondition()
+    var isThreadAvailable = false
+    var isStorageEmpty: Bool {
+        storage.isEmpty
+    }
+
+    func addElement(element: Chip) {
+        mutex.lock()
+        isThreadAvailable = true
+        storage.append(element)
+        mutex.signal()
+        mutex.unlock()
+    }
+
+    func removeElement() -> Chip {
+        mutex.lock()
+        while (!isThreadAvailable) {
+            mutex.wait()
+        }
+        isThreadAvailable = false
+        mutex.unlock()
+        return storage.removeLast()
     }
 }
 
 class GenerationThread: Thread {
-    func generateInstances() {
-        var counter = 0
-            let timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { timer in
-                mutex.lock()
-                storage.append(Chip.make())
-                counter += 1
-                print("Instance \(counter) added to the storage.")
-                mutex.unlock()
+    private var storage: ThreadSafeWorkout
+    private var timer = Timer()
+    var counter = 0
 
-                if counter == 10 {
-                   timer.invalidate()
-            }
-        }
-        RunLoop.main.add(timer, forMode: .default)
-        RunLoop.main.run(until: Date.init(timeIntervalSinceNow: 0))
+    init(storage: ThreadSafeWorkout) {
+        self.storage = storage
+    }
+
+    override func main() {
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(generateInstances), userInfo: nil, repeats: true)
+
+        RunLoop.current.add(timer, forMode: .default)
+        RunLoop.current.run(until: Date(timeIntervalSinceNow: 20))
+    }
+
+    @objc
+    func generateInstances() {
+        storage.addElement(element: Chip.make())
+        counter += 1
+        print("Chip \(counter) is added")
         }
     }
 
 class WorkingThread: Thread {
-    func workWithChip() {
-        var counter = 0
-        let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-            while (!isStorageEmpty) {
-                mutex.lock()
-                print(storage)
-                var instance = storage.removeLast()
-                instance.soldering()
-                counter += 1
-                print("Chip \(counter) is soldered.")
-                mutex.unlock()
-            }
-            RunLoop.current.add(timer, forMode: .default)
-            RunLoop.current.run(until: Date.init(timeIntervalSinceNow: 0))
-        }
+    private let storage: ThreadSafeWorkout
+    var counter = 0
+
+    init(storage: ThreadSafeWorkout) {
+        self.storage = storage
+    }
+
+    override func main() {
+                storage.isStorageEmpty
+                        repeat {
+                            storage.removeElement().soldering()
+                            counter += 1
+                            print("Chip \(counter) is soldered")
+                        }
+        while storage.isStorageEmpty || storage.isThreadAvailable
+//        while !storage.isStorageEmpty {
+//            storage.removeElement().soldering()
+//            counter += 1
+//            print("Chip \(counter) is soldered")
+//        }
     }
 }
 
-var generationQueue = GenerationThread()
-var workingQueue = WorkingThread()
+let storage = ThreadSafeWorkout()
+var generationQueue = GenerationThread(storage: storage)
+var workingQueue = WorkingThread(storage: storage)
 
-generationQueue.generateInstances()
-workingQueue.workWithChip()
+generationQueue.start()
+workingQueue.start()
+
+
+
 
